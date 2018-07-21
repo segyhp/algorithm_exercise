30196c6fe56d946410160f08004802b0f5527e00
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-07-21 16:38:14 +0700

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+//solutin one
+//function reverse(str) {
+//    
+////   const arra = str.split('');
+////    arra.reverse();
+////    return arra.join('');
+//    
+//    return str
+//        .split('') 
+//        .reverse()
+//        .join('')
+//}
+
+//solution two
+function reverse(str){
+    let reversed = '';
+    
+   for (let chara of str){
+       reversed = chara + reversed;
+       debugger;
+   }
+    
+    return reversed;
+}
+
+//function reverse(str){
+//    debugger;
+//    return str.split('').reduce((rev, char)=> char + rev, '')
+//}
+
+
+reverse('asdf')
+
 
 module.exports = reverse;
+ 

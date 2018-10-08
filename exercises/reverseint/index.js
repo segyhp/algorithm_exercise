96a8d9211ff2721fbbc6b718194d7c502c858069
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-10-08 15:20:22 +0700

    reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('')
+
+
+    // if(n < 0){
+         return parseInt(reversed) * Math.sign(n)
+    // }
+
+    // return parseInt(reversed)
+
+
+}
 
 module.exports = reverseInt;

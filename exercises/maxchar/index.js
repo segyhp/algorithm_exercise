f0d270adccc18f005ff285001f38acba9a523e89
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-10-08 16:32:52 +0700

    max character in string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+const charMap = {};
+let max = 0
+let maxChar = ''
+
+for(let char of str){
+    //builind a character map
+    //chars[char] = chars[char] + 1 || 1
+    if (!charMap[char]) {
+        charMap[char] = 1
+    }
+    else {
+        charMap[char]++
+    }
+ 
+}
+
+
+  for (let char in charMap) {
+      if (charMap[char] > max) {
+          max = charMap[char]
+          maxChar = char
+      }
+  }
+
+  return maxChar
+
+
+}
 
 module.exports = maxChar;

3928e479bc746b753447d95aafea0fced98270b7
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-10-09 12:14:57 +0700

    array chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  
+    const chunked = []
+    let idx = 0
+
+    while (idx < array.length){
+        chunked.push(array.slice(idx,idx+size))
+        idx  += size
+    }
+
+    return chunked
+
+
+
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//     const chunked = [];
+
+//     for (let element of array) {
+//         const last = chunked[chunked.length - 1]
+
+//         if (!last || last.length === size) {
+//             chunked.push([element])
+//         } else {
+//             last.push(element)
+//         }
+
+
+//     }
+
+//     return chunked
+// }
